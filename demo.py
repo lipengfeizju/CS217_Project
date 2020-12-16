1d0116ae98f50d570d70dfa092559a403191384b
Pengfei Li <pli081@ucr.edu>
2020-12-16 09:59:22 -0700

    small update

--- demo.py
+++ demo.py
@@ -2,51 +2,12 @@ import numpy as np
 import subprocess
 import os 
 from scipy.spatial.transform import Rotation as R
-from utils.visualize import visualize_point_cloud
-
-def load_pcl_open3d(pcl_path, point_color =[0.1, 0.5, 0.2]):
-    '''
-    Load PCL data, return point cloud array and color array
-    '''
-    assert os.path.exists(pcl_path), "File not exists! " + pcl_path + "\n"
-    point_set = np.loadtxt(pcl_path, delimiter=',').astype(np.float32)
-    point_set = point_set[:,:3]
-    num_point = point_set.shape[0]
-    color_array  = np.repeat(point_color, num_point).reshape([3, num_point]).astype(np.float64).T
-
-    return point_set, color_array
-
-def random_transform(sigma_r = 0.5, sigma_t = 0.5):
-    from scipy.spatial.transform import Rotation as R
-    r1 = np.random.uniform(-1,1,3)
-    r1 = r1/np.sqrt(np.sum(r1**2))*sigma_r
-    rot_mat = R.from_rotvec(r1).as_matrix()
-
-    t1 = np.random.uniform(-1,1,3)
-    t1 = t1/np.sqrt(np.sum(t1**2))*sigma_t
-
-    transform = np.zeros([3,4])
-    transform[:3, :3] = rot_mat
-    transform[:3, 3] = t1
-
-    return transform
-
-def save_transformed_data(point_original, transform, file_path):
-    '''
-    transform the original point cloud
-    '''
-
-    assert point_original.shape[1] == 3
-    num_point = point_original.shape[0]
-    point_set = np.ones((num_point, 4))
-    point_set[:,:3] = point_original
-    point_set = transform @ point_set.T
-    np.savetxt(file_path, point_set.T, delimiter=",", fmt="%.6f")
+from utils.visualize import visualize_point_cloud, random_transform, save_transformed_data, load_pcl_open3d
 
 def main():
     base_dir = "/home/anny/cuda-workspace/icp_project/icp/data/samples/"
-    pcl_original = "airplane_0002.txt"
-    pcl_transformed = "airplane_0002_rotated.txt"
+    pcl_original = "airplane_0001.txt"
+    pcl_transformed = "airplane_0001_rotated.txt"
     pcl_recoverd = "recovered.txt"
 
     subprocess.run(["./build/registration", base_dir , pcl_original, pcl_transformed])
@@ -62,15 +23,15 @@ def main():
     color_array = np.concatenate((color_array, color_array2, color_array3), axis=0)
     visualize_point_cloud(point_set, color_array)
 
-def save_test():
+def random_transform_data():
     base_dir = "/home/anny/cuda-workspace/icp_project/icp/data/samples/"
-    pcl_original = "airplane_0002.txt"
+    pcl_original = "airplane_0001.txt"
 
     point_set, color_array = load_pcl_open3d(base_dir + pcl_original, point_color=[0.1, 0.5, 0.2])
 
     transform = random_transform()
-    save_transformed_data(point_set, transform, base_dir + "airplane_0002_rotated.txt")
+    save_transformed_data(point_set, transform, base_dir + "airplane_0001_rotated.txt")
 
 if __name__ == "__main__":
     main()
-    # save_test()
+    # random_transform_data()
